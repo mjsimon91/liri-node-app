51c4b8b4b751e7038151d8cdd8713a5df11ca0c3
mjsimon91 <mjsimon91@gmail.com>
2018-02-02 22:02:44 -0500

    Spotify search is now working

--- liri.js
+++ liri.js
@@ -11,7 +11,8 @@ var client = new Twitter(keys.twitter);
 //creating a variable for the argument
 
 var arg = process.argv
-var userTweet
+var userTweet;          // This will store the tweets from the user
+var songName =arg[3];       // This is the name of the song that the user enters
 //Get the argumet for the command
 
 var command = arg[2];
@@ -19,6 +20,8 @@ var command = arg[2];
 // show your last 20 tweets and when they were created
   if (command == "tweets") {
     twitter();
+  } else if (command == "song") {
+    spotifySong();
   }
 //This will show the following information about the song
 
@@ -48,3 +51,24 @@ function twitter(){
     } // End the else statement
   }) //End the call to Twitter
 } //End the twitter function
+
+//Creating a function for when the user requests information about a song
+
+function spotifySong(){
+
+  //Creating a loop that will be able to consume a ful song name
+
+  for( var i = 4; i < arg.length; i++){
+    songName = songName + "+" + arg[i];
+  }
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+      if (err) {
+      return console.log('Error occurred: ' + err);
+    }else {
+      // for (var i = 0; i < data.tracks.items.length; i++) {
+      // console.log(data.tracks.items[i])
+      // }
+      console.log(JSON.stringify(data,null,2));
+    }
+    });
+}
